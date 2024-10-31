e0003a6242b579c302e601cd27fd0a398ed55391
John Lago <>
2024-10-31 03:48:06 +0000

    Update to Todo App

--- main.py
+++ main.py
@@ -1,7 +1,56 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+# PostgreSQL database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://todo_user:todo_password@localhost/todo_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Todo Model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Todo {self.title}>'
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
 def index():
-    return "<h1>Hello from Jetify!</h1>"
+    todos = Todo.query.order_by(Todo.created_at.desc()).all()
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    if title:
+        new_todo = Todo(title=title)
+        db.session.add(new_todo)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:id>')
+def complete(id):
+    todo = Todo.query.get_or_404(id)
+    todo.completed = not todo.completed
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    todo = Todo.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
